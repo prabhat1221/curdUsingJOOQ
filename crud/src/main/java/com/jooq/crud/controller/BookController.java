5e22b54010798cf944933488c855763f7eeb3ec3
prabhat1221 <prabhatroy.info@gmail.com>
2021-07-08 09:57:52 +0530

    CRUD Using JOOQ v 1.0

--- crud/src/main/java/com/jooq/crud/controller/BookController.java
+++ crud/src/main/java/com/jooq/crud/controller/BookController.java
@@ -3,8 +3,11 @@ package com.jooq.crud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +28,20 @@ public class BookController {
     public void postBook(@RequestBody Book book){
         this.bookService.insertBook(book);
     }
-	
+    
+//    @GetMapping("/getBookById/{id}")
+//    public Result<Record> getBookById(@PathVariable Integer id){
+//        return this.bookService.getBookById(id);
+//    }
+    
+    @DeleteMapping("/deleteBook/{id}")
+    public void deleteById(@PathVariable Integer id) {
+    	this.bookService.deleteById(id);
+    }
+    
+    @PutMapping("/update")
+    public void update(@RequestBody Book book){
+    	this.bookService.update(book);
+    }
 	
 }
--- crud/src/main/java/com/jooq/crud/service/BookService.java
+++ crud/src/main/java/com/jooq/crud/service/BookService.java
@@ -3,11 +3,14 @@ package com.jooq.crud.service;
 import java.util.List;
 
 import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jooq.crud.model.Tables;
 import com.jooq.crud.model.tables.pojos.Book;
+import com.jooq.crud.model.tables.records.BookRecord;
 
 @Service
 public class BookService {
@@ -21,13 +24,28 @@ public class BookService {
                 .fetchInto(Book.class);
     }
     public void insertBook(Book book){
-        context
-                .insertInto(Tables.BOOK, Tables.BOOK.TITLE,
-                            Tables.BOOK.AUTHOR)
-                .values(book.getTitle(), book.getAuthor())
-                .execute();
+    	BookRecord book1 = context.newRecord(Tables.BOOK);
+    	book1.setTitle(book.getTitle());
+    	book1.setAuthor(book.getAuthor());
+    	book1.store();
+    }
+//    public Result<Record> getBookById(Integer id) {
+//    	return context.select()
+//    			 .from(Tables.BOOK)
+//    			 .where(Tables.BOOK.ID.eq(1))
+//    			 .fetch();
+//    }
+    public void deleteById(Integer id) {
+    	BookRecord book = context.fetchOne(Tables.BOOK, Tables.BOOK.ID.eq(id));
+    	book.delete();
     }
     
+    public void update(Book book) {
+    	BookRecord book1 = context.fetchOne(Tables.BOOK, Tables.BOOK.ID.eq(book.getId()));
+    	book1.setTitle(book.getTitle());
+    	book1.setAuthor(book.getAuthor());
+    	book1.store();
+    }
     
-    
+
 }
